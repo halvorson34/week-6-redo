16b48aafe7c950fed1b7ad9cd1e38faf9904a10c
Matt Halvorson <halvorson34.mh@gmail.com>
2020-03-08 23:14:15 -0500

    attempted button function and failed miserably

--- assignment/scripts/client.js
+++ assignment/scripts/client.js
@@ -3,6 +3,12 @@ let garage = [];
 /*
 Do not change newCar for base mode!
 */
+function addCarButton(){
+  let outputText = $( '#carInput' );
+  $( '#addCarButton' ).on( 'click', newCar );
+  console.log( outputText );
+}
+
 function newCar(yearInput, makeInput, modelInput){
   console.log('in newCar:', yearInput, makeInput, modelInput);
   const newCarObject = {
